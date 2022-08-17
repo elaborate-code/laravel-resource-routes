368541d612ee969ac25216af990d7c44837a4dee
medilies <medilies@gmail.com>
2022-08-17 17:45:21 +0100

    FEAT: ResourceRoute class

--- src/Facades/ResourceRoute.php
+++ src/Facades/ResourceRoute.php
@@ -5,6 +5,14 @@ namespace ElaborateCode\ResourceRoute\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Illuminate\Routing\Route index($name, $controller)
+ * @method static \Illuminate\Routing\Route store($name, $controller)
+ * @method static \Illuminate\Routing\Route create($name, $controller)
+ * @method static \Illuminate\Routing\Route show($name, $controller)
+ * @method static \Illuminate\Routing\Route update($name, $controller)
+ * @method static \Illuminate\Routing\Route destroy($name, $controller)
+ * @method static \Illuminate\Routing\Route edit($name, $controller)
+ *
  * @see \ElaborateCode\ResourceRoute\ResourceRoute
  */
 class ResourceRoute extends Facade
--- src/ResourceRoute.php
+++ src/ResourceRoute.php
@@ -2,6 +2,91 @@
 
 namespace ElaborateCode\ResourceRoute;
 
-class ResourceRoute
+use Illuminate\Routing\Router;
+
+class ResourceRoute extends Router
 {
+    /**
+     * Route a resource to a controller index method.
+     *
+     * @param  string  $name
+     * @param  string  $controller
+     * @return \Illuminate\Routing\PendingResourceRegistration
+     */
+    public function index($name, $controller)
+    {
+        return $this->resource($name, $controller, ['only' => __FUNCTION__]);
+    }
+
+    /**
+     * Route a resource to a controller store method.
+     *
+     * @param  string  $name
+     * @param  string  $controller
+     * @return \Illuminate\Routing\PendingResourceRegistration
+     */
+    public function store($name, $controller)
+    {
+        return $this->resource($name, $controller, ['only' => __FUNCTION__]);
+    }
+
+    /**
+     * Route a resource to a controller create method.
+     *
+     * @param  string  $name
+     * @param  string  $controller
+     * @return \Illuminate\Routing\PendingResourceRegistration
+     */
+    public function create($name, $controller)
+    {
+        return $this->resource($name, $controller, ['only' => __FUNCTION__]);
+    }
+
+    /**
+     * Route a resource to a controller show method.
+     *
+     * @param  string  $name
+     * @param  string  $controller
+     * @return \Illuminate\Routing\PendingResourceRegistration
+     */
+    public function show($name, $controller)
+    {
+        return $this->resource($name, $controller, ['only' => __FUNCTION__]);
+    }
+
+    /**
+     * Route a resource to a controller update method.
+     *
+     * @param  string  $name
+     * @param  string  $controller
+     * @return \Illuminate\Routing\PendingResourceRegistration
+     */
+    public function update($name, $controller)
+    {
+        return $this->resource($name, $controller, ['only' => __FUNCTION__]);
+    }
+
+    /**
+     * Route a resource to a controller destroy method.
+     *
+     * @param  string  $name
+     * @param  string  $controller
+     * @return \Illuminate\Routing\PendingResourceRegistration
+     */
+    public function destroy($name, $controller)
+    {
+        return $this->resource($name, $controller, ['only' => __FUNCTION__]);
+    }
+
+    /**
+     * Route a resource to a controller edit method.
+     *
+     * @param  string  $name
+     * @param  string  $controller
+     * @return \Illuminate\Routing\PendingResourceRegistration
+     */
+    public function edit($name, $controller)
+    {
+        return $this->resource($name, $controller, ['only' => __FUNCTION__]);
+    }
 }
